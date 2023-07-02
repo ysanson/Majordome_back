d39c6ab442c9c808fe3982c54e33cf86cb9c275b
Yvan SANSON <sansonyvan@gmail.com>
2023-07-02 21:16:44 +0200

    Cloned https://github.com/javierlecca/go-grpc-beautiful-architecture to have a nice start

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import "github.com/jinzhu/configor"
+
+type Config struct {
+	AppName string `default:"grcp"`
+	Port    int32  `default:"8000"`
+	DB      struct {
+		Use   string `default:"postgres"`
+		Mysql []struct {
+			Enabled  bool   `default:"true"`
+			Host     string `default:"localhost"`
+			Port     string `default:"3306"`
+			UserName string `default:"root"`
+			Password string `default:"xxxxxxxx"`
+			Database string `default:"xxxxxxxx"`
+		}
+		Postgres []struct {
+			Enabled  bool   `default:"true"`
+			Host     string `default:"localhost"`
+			Port     string `default:"5432"`
+			UserName string `default:"postgres"`
+			Password string `default:"xxxxxxxx"`
+			Database string `default:"Majordome"`
+		}
+	}
+}
+
+func (c *Config) NewConfig() (*Config, error) {
+	err := configor.Load(c, "config.yml")
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
